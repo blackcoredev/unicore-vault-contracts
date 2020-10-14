80c46758fd67b2b043bfe052d56f52aa09262b64
Unicore-Dev <72827460+Unicore-Dev@users.noreply.github.com>
2020-10-14 16:26:30 -0600

    Create wUNIv2.sol

---
+++ contracts/wUNIv2.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: WHO GIVES A FUCK ANYWAY??
+
+pragma solidity >=0.6.0;
+
+import "./ERC20.sol";
+
+//Liquidity Token Wrapper
+
+contract wUNIV2 is ERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    address public UniCore;
+    address public UNIv2;
+
+    
+//=========================================================================================================================================
+
+    constructor(address _UniCore, address _UNIv2) ERC20("Wrapped UniCoreLP","wUNIv2") public {
+        UniCore = _UniCore;
+        UNIv2 = _UNIv2;
+    }
+
+//=========================================================================================================================================
+    //WUNIv2 minter
+    function _wrapUNIv2(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        
+        //Get UniCore Balance of UNIv2
+        uint256 UNIv2Balance = ERC20(UNIv2).balanceOf(UniCore);
+        
+        //transferFrom UNIv2
+        ERC20(UNIv2).transferFrom(sender, recipient, amount);
+        
+        //Mint Tokens, equal to the UNIv2 amount sent
+        _mint(sender, amount);
+
+        //Checks if balances OK otherwise throw
+        require(UNIv2Balance.add(amount) == ERC20(UNIv2).balanceOf(UniCore), "Math Broken");
+    }
+    
+    function wrapUNIv2(uint256 amount) public {
+        _wrapUNIv2(msg.sender, UniCore, amount);
+    }
+     
+
+}
